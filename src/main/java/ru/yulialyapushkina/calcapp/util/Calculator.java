c8c2f9884539512388ddb3d3f81a8b59bffad319
Julia1284 <julia123456-84@mail.ru>
2024-02-11 14:59:27 +0300

    add magins in class Calculator

--- src/main/java/ru/yulialyapushkina/calcapp/util/Calculator.java
+++ src/main/java/ru/yulialyapushkina/calcapp/util/Calculator.java
@@ -4,9 +4,11 @@ public class Calculator {
     public static int sumCalculate(int num1, int num2) {
         return num1 + num2;
     }
+
     public static int diffCalculate(int num1, int num2) {
         return num1 - num2;
     }
+
     public static int productCalculate(int num1, int num2) {
         return num1 * num2;
     }
