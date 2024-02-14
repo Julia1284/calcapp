48114baa2bd08e5afa8306ad8f67fc9f378f675a
Julia1284 <julia123456-84@mail.ru>
2024-02-14 21:42:19 +0300

    change names of method in class Calculator

--- src/main/java/ru/yulialyapushkina/calcapp/_main/_Main.java
+++ src/main/java/ru/yulialyapushkina/calcapp/_main/_Main.java
@@ -7,9 +7,9 @@ public class _Main {
     public static void main(String[] args) {
         int num1 = 12;
         int num2 = 6;
-        int result1 = Calculator.sumCalculate(num1,num2);
-        int result2 = Calculator.diffCalculate(num1,num2);
-        int result3 = Calculator.productCalculate(num1,num2);
+        int result1 = Calculator.calcSum(num1,num2);
+        int result2 = Calculator.calcDiff(num1,num2);
+        int result3 = Calculator.calcProd(num1,num2);
         ResultWriterService.printResult(num1, num2, result1, "sum");
         ResultWriterService.printResult(num1, num2, result2, "difference");
         ResultWriterService.printResult(num1, num2, result3, "product");
--- src/main/java/ru/yulialyapushkina/calcapp/util/Calculator.java
+++ src/main/java/ru/yulialyapushkina/calcapp/util/Calculator.java
@@ -1,15 +1,15 @@
 package ru.yulialyapushkina.calcapp.util;
 
 public class Calculator {
-    public static int sumCalculate(int num1, int num2) {
+    public static int calcSum(int num1, int num2) {
         return num1 + num2;
     }
 
-    public static int diffCalculate(int num1, int num2) {
+    public static int calcDiff(int num1, int num2) {
         return num1 - num2;
     }
 
-    public static int productCalculate(int num1, int num2) {
+    public static int calcProd(int num1, int num2) {
         return num1 * num2;
     }
 }
